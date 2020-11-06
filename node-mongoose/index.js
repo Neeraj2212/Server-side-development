441a4ff4c00e4a1da0c5408dfae7e9836a950317
Neeraj Patil <patilneeraj2212@gmail.com>
2020-11-07 00:26:02 +0530

    mongoose odm part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -13,13 +13,10 @@ const connect = mongoose.connect(url, {
 connect.then((db) => {
 	console.log("Connected correctly to server");
 
-	var newDish = Dishes({
+	Dishes.create({
 		name: "Uthappizza",
 		description: "test",
-	});
-
-	newDish
-		.save()
+	})
 		.then((dish) => {
 			console.log(dish);
 
