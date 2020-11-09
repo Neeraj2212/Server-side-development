dc7c52289f774af3309f9fa8ea0e5339904a80c3
Neeraj Patil <patilneeraj2212@gmail.com>
2020-11-09 16:32:20 +0530

    Passport-JWT

---
+++ conFusionServer/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+	secretKey: "mumbal-jumbal",
+	mongoUrl: "mongodb://localhost:27017/conFusion",
+};
--- conFusionServer/routes/leaderRouter.js
+++ conFusionServer/routes/leaderRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const authenticate = require("../authenticate");
 const Leaders = require("../models/leaders");
 
 const leaderRouter = express.Router();
@@ -22,7 +22,7 @@ leaderRouter
 			)
 			.catch((err) => next(err));
 	})
-	.post((req, res, next) => {
+	.post(authenticate.verifyUser, (req, res, next) => {
 		Leaders.create(req.body)
 			.then(
 				(leader) => {
@@ -35,11 +35,11 @@ leaderRouter
 			)
 			.catch((err) => next(err));
 	})
-	.put((req, res, next) => {
+	.put(authenticate.verifyUser, (req, res, next) => {
 		res.statusCode = 403;
 		res.end("PUT operation not supported on /leaders");
 	})
-	.delete((req, res, next) => {
+	.delete(authenticate.verifyUser, (req, res, next) => {
 		Leaders.remove({})
 			.then(
 				(resp) => {
@@ -66,7 +66,7 @@ leaderRouter
 			)
 			.catch((err) => next(err));
 	})
-	.post((req, res, next) => {
+	.post(authenticate.verifyUser, (req, res, next) => {
 		res.statusCode = 403;
 		res.end("POST operation not supported on /leaders/" + req.params.leaderId);
 	})
@@ -88,7 +88,7 @@ leaderRouter
 			)
 			.catch((err) => next(err));
 	})
-	.delete((req, res, next) => {
+	.delete(authenticate.verifyUser, (req, res, next) => {
 		Leaders.findByIdAndRemove(req.params.leaderId)
 			.then(
 				(resp) => {
