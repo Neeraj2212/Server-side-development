647957775fae9520ad72e24be21f5db6507f5ee3
Neeraj Patil <patilneeraj2212@gmail.com>
2020-11-13 01:53:36 +0530

    facebook auth Error in index

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,10 @@
+require("dotenv").config();
+
 module.exports = {
 	secretKey: "12345-67890-09876-54321",
 	mongoUrl: "mongodb://localhost:27017/conFusion",
+	facebook: {
+		clientId: process.env.clientId,
+		clientSecret: process.env.clientSecret,
+	},
 };
